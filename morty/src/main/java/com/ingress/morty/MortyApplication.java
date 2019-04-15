ecfff8ea6d2221389454e20baa265bfb2ccf3b56
Mustafa Seckin Ozden <mustafaseckin.oezden@metronom.com>
2019-04-15 23:26:31 +0200

    Ingress rules, deployment scripts prepared, rick and morty services implemented.

---
+++ morty/src/main/java/com/ingress/morty/MortyApplication.java
@@ -0,0 +1,39 @@
+package com.ingress.morty;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class MortyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MortyApplication.class, args);
+  }
+
+
+  @GetMapping("/")
+  public ResponseEntity<byte[]> welcome() throws IOException {
+    File img = new File("morty.png");
+    return ResponseEntity
+        .ok()
+        .contentType(MediaType.IMAGE_PNG)
+        .body(Files.readAllBytes(img.toPath()));
+  }
+
+  @GetMapping("/hello")
+  public ResponseEntity<byte[]> hello() throws IOException {
+    File img = new File("morty.gif");
+    return ResponseEntity
+        .ok()
+        .contentType(MediaType.IMAGE_GIF)
+        .body(Files.readAllBytes(img.toPath()));
+  }
+}
